de8c5719b03281e74181fcbc5eb10d9744e999f0
Alex Wang <aswang96@stanford.edu>
2017-03-11 22:00:49 -0800

    finished cipers option

--- scurl.py
+++ scurl.py
@@ -5,11 +5,6 @@ import argparse
 import OpenSSL
 import socket
 
-def t1():
-    print "in t1"
-
-def t2():
-    print "in t2"
 
 def main():
     parser = argparse.ArgumentParser()
@@ -18,18 +13,35 @@ def main():
     parser.add_argument("--tlsv1.2", action="store_true")
     parser.add_argument("--sslv3", action="store_true")
     parser.add_argument("-3",action="store_true")
+    parser.add_argument("--ciphers")
     args = parser.parse_args()
     var_args = vars(args)
 
+    method = ""
     if var_args["tlsv1.0"]:
-        t1()
+        method = OpenSSL.SSL.TLSv1_METHOD
+    elif var_args["tlsv1.1"]:
+        method = OpenSSL.SSL.TLSv1_1_METHOD
+    elif var_args["tlsv1.2"]:
+        method = OpenSSL.SSL.TLSv1_2_METHOD
+    elif var_args["sslv3"]:
+        method = OpenSSL.SSL.SSLv23_METHOD #SSLV3 might be deprecated in newest OpenSSL, switch back and test on corn
+    elif var_args["3"]:
+        method = OpenSSL.SSL.SSLv23_METHOD
     else:
-        t2()
+        method = OpenSSL.SSL.TLSv1_2_METHOD
 
     # Create socket and context, and setup connection
-    context = OpenSSL.SSL.Context(OpenSSL.SSL.SSLv23_METHOD) # method defined by command line options
+    context = OpenSSL.SSL.Context(method) # method defined by command line options
+    # Check for cipher list and handle appropriately
+    if var_args["ciphers"]:
+        try:
+            context.set_cipher_list(var_args["ciphers"])
+        except:
+            context = OpenSSL.SSL.Context(method)
+
     connection = OpenSSL.SSL.Connection(context, socket.socket())
-    connection.connect(("www.google.com", 443)) # defualt 443
+    connection.connect(("www.google.com", 443)) # default 443
     connection.do_handshake()
     connection.send("GET / HTTP/1.0\r\nHost: www.google.com\r\nUser-Agent: CryptoGods\r\n\r\n")
     data = ""
@@ -39,7 +51,7 @@ def main():
             data += new_data
         except OpenSSL.SSL.Error:
             break
-    print data
+
     connection.shutdown()
     connection.close()
     print data
